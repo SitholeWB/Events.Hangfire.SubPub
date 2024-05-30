ec63efd02b83c22ec4fcc06c0684475d156bfbc3
Welcome Sithole <sitholewb@gmail.com>
2024-05-30 15:32:50 +0200

    Add more tests

--- SubPub.Hangfire.Tests/ScheduleUnitTests.cs
+++ SubPub.Hangfire.Tests/ScheduleUnitTests.cs
@@ -56,8 +56,36 @@ namespace SubPub.Hangfire.Tests
             );
 
             // Assert
-            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Method.Name == "RunAsync"), It.IsAny<EnqueuedState>()), Times.Never);
-            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(TestHandler) && job.Method.Name == "RunAsync"), It.IsAny<EnqueuedState>()), Times.Never);
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Never);
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(TestHandler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Never);
+        }
+
+        [Fact]
+        public void Schedule_RunAsync_OneEventAndOneHandler_ShouldCallRunAsyncOnce()
+        {
+            // Arrange
+            _services.AddHangfireSubPub<TestEvent>()
+                     .Subscribe<TestHandler>();
+            _serviceProvider.Setup(s => s.GetService(typeof(TestHandler)))
+                     .Returns(new TestHandler());
+
+            var provider = _services.BuildServiceProvider();
+            var _hangfireEventHandlerContainer = provider.GetRequiredService<IHangfireEventHandlerContainer>();
+
+            // Act
+            _hangfireEventHandlerContainer.Publish(new TestEvent
+            {
+                Name = "Bob"
+            },
+            new HangfireJobOptions
+            {
+                HangfireJobType = HangfireJobType.Schedule,
+                TimeSpan = TimeSpan.FromSeconds(15),
+            });
+
+            // Assert
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(TestHandler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()));
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()));
         }
     }
 }
