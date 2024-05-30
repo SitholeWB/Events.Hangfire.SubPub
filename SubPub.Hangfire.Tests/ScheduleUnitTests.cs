6d7ffd35c6f2baf5ec558cf627a71c484507e518
Welcome Sithole <sitholewb@gmail.com>
2024-05-30 15:34:44 +0200

    Add more tests

--- SubPub.Hangfire.Tests/ScheduleUnitTests.cs
+++ SubPub.Hangfire.Tests/ScheduleUnitTests.cs
@@ -61,6 +61,34 @@ namespace SubPub.Hangfire.Tests
         }
 
         [Fact]
+        public void Schedule_RunAsync_Schedule_OneEventAndOneHandler_ShouldNotEnqueue()
+        {
+            // Arrange
+            _services.AddHangfireSubPub<TestEvent>()
+                     .Subscribe<TestHandler>();
+            _serviceProvider.Setup(s => s.GetService(typeof(TestHandler)))
+                     .Returns(new TestHandler());
+
+            var provider = _services.BuildServiceProvider();
+            var _hangfireEventHandlerContainer = provider.GetRequiredService<IHangfireEventHandlerContainer>();
+
+            // Act
+            _hangfireEventHandlerContainer.Publish(new TestEvent
+            {
+                Name = "Bob"
+            },
+            new HangfireJobOptions
+            {
+                HangfireJobType = HangfireJobType.Schedule,
+                TimeSpan = TimeSpan.FromSeconds(15),
+            });
+
+            // Assert
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(TestHandler) && job.Method.Name == "RunAsync"), It.IsAny<EnqueuedState>()), Times.Never);
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Method.Name == "RunAsync"), It.IsAny<EnqueuedState>()), Times.Never);
+        }
+
+        [Fact]
         public void Schedule_RunAsync_OneEventAndOneHandler_ShouldCallRunAsyncOnce()
         {
             // Arrange
