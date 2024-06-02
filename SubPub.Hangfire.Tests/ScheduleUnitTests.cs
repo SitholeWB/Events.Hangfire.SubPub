a3e15335f31f11911ccea6504beee0d2cabac7a2
Welcome Sithole <sitholewb@gmail.com>
2024-06-02 12:59:46 +0200

    Add more tests

--- SubPub.Hangfire.Tests/ScheduleUnitTests.cs
+++ SubPub.Hangfire.Tests/ScheduleUnitTests.cs
@@ -170,5 +170,43 @@ namespace SubPub.Hangfire.Tests
             _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(Test2Handler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Once());
             _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(AnotherHandler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Once());
         }
+
+        [Fact]
+        public void Schedule_RunAsync_TwoEventsAndThreeHandlers_BUT_PublishToOneHandler_ShouldCallRunAsyncOnce()
+        {
+            // Arrange
+            _services.AddHangfireSubPub<TestEvent>()
+                     .Subscribe<TestHandler>()
+                     .Subscribe<Test2Handler>();
+            _services.AddHangfireSubPub<AnotherEvent>()
+                     .Subscribe<AnotherHandler>();
+
+            _serviceProvider.Setup(s => s.GetService(typeof(TestHandler)))
+                     .Returns(new TestHandler());
+            _serviceProvider.Setup(s => s.GetService(typeof(Test2Handler)))
+                     .Returns(new Test2Handler());
+            _serviceProvider.Setup(s => s.GetService(typeof(AnotherHandler)))
+                     .Returns(new AnotherHandler());
+
+            var provider = _services.BuildServiceProvider();
+            var _hangfireEventHandlerContainer = provider.GetRequiredService<IHangfireEventHandlerContainer>();
+
+            // Act
+            _hangfireEventHandlerContainer.Publish(new AnotherEvent
+            {
+                Name = "Bob"
+            },
+            new HangfireJobOptions
+            {
+                HangfireJobType = HangfireJobType.Schedule,
+                TimeSpan = TimeSpan.FromSeconds(15),
+            });
+
+            // Assert
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Exactly(1));
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(TestHandler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Never());
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(Test2Handler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Never());
+            _backgroundJobClient.Verify(x => x.Create(It.Is<Job>(job => job.Type == typeof(AnotherHandler) && job.Method.Name == "RunAsync"), It.IsAny<ScheduledState>()), Times.Once());
+        }
     }
 }
